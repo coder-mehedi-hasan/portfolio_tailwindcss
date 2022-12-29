2846b4b950840c9e7200a2a51a33922fa8b2c76e
Md Mehedi Hasan <mh8399807@gmail.com>
2022-12-30 02:49:51 +0600

    upload

--- js/app.js
+++ js/app.js
@@ -1,22 +1,90 @@
 let section_slider = document.getElementsByClassName('section_slider')
-let sliderDot = document.getElementsByClassName('sliderDot')
-let defaultValue = 0;
-section_sliderFun(defaultValue)
+let sectionSliderDot = document.getElementsByClassName('secSliderDot')
+let addNavbar = document.getElementById('addNavbar')
+let removeNavbar = document.getElementById('removeNavbar')
+let main_navTop = document.getElementById('main_nav')
+let removeNavbarIcon = document.getElementById('removeNavbarIcon')
 
-function clickElement(element){
-    section_sliderFun(defaultValue = element)
+let ol = document.querySelectorAll('ol li a')
+console.log(ol)
+
+addNavbar.addEventListener('click',function(){
+    main_navTop.style.top='0px'
+})
+removeNavbar.addEventListener('click',function(){
+    main_navTop.style.top='-100%'
+})
+if(main_navTop.style.top == '-100%'){
+    main_navTop.style.visibility='hidden'
+}
+removeNavbar.addEventListener('mouseenter',function(){
+    if(removeNavbarIcon.style.transform === 'rotate(0deg)'){
+        removeNavbarIcon.style.transform = 'rotate(450deg)'
+        removeNavbarIcon.style.transition = '1s'
+    }else{
+        removeNavbarIcon.style.transform = 'rotate(0deg)'
+    }
+})
+// document.getElementById('secSlideDotsGroup').style.visibility = 'hidden'
+function sectionSliderFunc(v){
+    for(i=0;i<v;i++){
+        section_slider[i].style.top = '-100%'
+    }
+    for(i=0;i<sectionSliderDot.length;i++){
+        sectionSliderDot[i].className = sectionSliderDot[i].className.replace(" active","")
+    }
+    sectionSliderDot[v].className+= ' active';
+    section_slider[v].style.top = '0px';
+    if(v>=1){
+        document.getElementById('secSlideDotsGroup').style.visibility = 'visible'
+    }else{
+        document.getElementById('secSlideDotsGroup').style.visibility = 'hidden'
+    }
 }
 
-section_slider.style.z
-function section_sliderFun(element){
-    for(i = 0;i<section_slider.length;i++){
-        section_slider[i].style.top = '-100%';
+window.addEventListener('wheel',function(event){
+    let v = 0
+    if(event.deltaY<0){
+        sectionSliderFunc(v)
+    }else{
+        sectionSliderFunc(v+=1)
     }
-    for(i=0;i<sliderDot.length;i++){
-        sliderDot[i].className = sliderDot[i].className.replace("active","")
-    }
-    section_slider[element].style.top = '0'
-    section_slider[element].style.zIndex = element++
-    section_slider[element].style.transition = '0.5s ease-in'
-    sliderDot[element-1].className += " active"
-}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// section_sliderFun(defaultValue)
+
+// function clickElement(element){
+//     section_sliderFun(defaultValue = element)
+// }
+
+// section_slider.style.z
+// function section_sliderFun(element){
+//     for(i = 0;i<section_slider.length;i++){
+//         section_slider[i].style.top = '-100%';
+//     }
+//     for(i=0;i<sliderDot.length;i++){
+//         sliderDot[i].className = sliderDot[i].className.replace("active","")
+//     }
+//     section_slider[element].style.top = '0'
+//     section_slider[element].style.zIndex = element++
+//     section_slider[element].style.transition = '0.5s ease-in'
+//     sliderDot[element-1].className += " active"
+// }
