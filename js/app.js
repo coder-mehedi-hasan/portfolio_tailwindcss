04397533bf31c7f54f61efcc5065eb5d6f86b2f5
Md Mehedi Hasan <mh8399807@gmail.com>
2023-01-01 18:26:48 +0600

    edit

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ let main_navTop = document.getElementById('main_nav')
 let removeNavbarIcon = document.getElementById('removeNavbarIcon')
 
 
-/*
 addNavbar.addEventListener('click',function(){
     main_navTop.style.top='0px'
 })
@@ -24,8 +23,7 @@ removeNavbar.addEventListener('mouseenter',function(){
         removeNavbarIcon.style.transform = 'rotate(0deg)'
     }
 })
-*/
-// document.getElementById('secSlideDotsGroup').style.visibility = 'hidden'
+document.getElementById('secSlideDotsGroup').style.visibility = 'hidden'
 function sectionSliderFunc(v){
     for(i=0;i<v;i++){
         section_slider[i].style.top = '-100%'
@@ -59,7 +57,6 @@ const timelineControlerBtnNext = document.querySelector('#timeline .next .slideB
 const timelineControler = document.querySelector('.timeline_slider .controler')
 let filler = document.querySelector('.controler .filler')
 
-// console.log(filler.style.width)
 timelineControlerBtnPrev.addEventListener('click',function(){
     timelineControler.style.transform = 'translateX(0)' 
     timelineControlerBtnPrev.className += ' inactiveControler'
@@ -78,8 +75,6 @@ const timelineSliderButtonPressed = e => {
         if(ButtonDate === SliderContentDate){
             timelineSliderContent.className += 'selected'
         }
-        timelineSliderContent.style.left='20px'
-        // console.log(m)
     } 
 }
 for(button of timelineSliderButtons){
@@ -87,10 +82,47 @@ for(button of timelineSliderButtons){
 }
 
 
+// portfolio 
+const porfolioNext = document.querySelector('.potfolio_slide_controler .next')
+const porfolioPrev = document.querySelector('.potfolio_slide_controler .prev')
+const portfolioSlide = document.querySelector('#portfolio .portfolio_slide')
+let defaultValue = 0
+porfolioNext.addEventListener('click',function(){
+    defaultValue++
+    if (defaultValue> 8 ){
+        defaultValue = 8
+    }
+    portfolioSlide.style.transform = `translate3d(${defaultValue * -410}px , 0px ,0px)`
+})
+porfolioPrev.addEventListener('click',function(){
+    defaultValue-=1
+    if(defaultValue < 0){
+        defaultValue=0
+    }
+    portfolioSlide.style.transform = `translate3d(${defaultValue * -410}px , 0px ,0px)`
+})
 
 
+// testimonial 
+
+const testimonialSliders = document.querySelectorAll('.testimonial_item .testimonial_slider .slider_item')
+const testimonialSliderControls= document.querySelectorAll('.testimonial_item .controler span')
 
 
+const testimonialCotrolClick = e =>{
+    const clickedData = e.target.getAttribute('data')
+    for(testimonialSlider of testimonialSliders){
+        const sliderData = testimonialSlider.getAttribute('data')
+        testimonialSlider.className = testimonialSlider.className.replace(" active","")
+        if(clickedData===sliderData){
+            testimonialSlider.className += ' active'
+        }
+    }
+}
+
+for(button of testimonialSliderControls){
+    button.addEventListener('click',testimonialCotrolClick)
+}
 
 
 
